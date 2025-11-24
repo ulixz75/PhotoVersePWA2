458a999a2dbf476d2ba7d38076d833f0608920e5
ulixz75 <136820760+ulixz75@users.noreply.github.com>
2025-11-24 13:17:12 +0100

    Update and rename service-worker.js to public/service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker instalado");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activado");
+});
+
+self.addEventListener("fetch", (event) => {
+  // Puedes personalizar el caché si quieres
+});
