60c8b921b723ac13b7a2cddeb632db7a7102dfbd
ulixz75 <136820760+ulixz75@users.noreply.github.com>
2025-11-24 15:58:57 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "shoukigaigoors.net",
-    "zoneId": 9939602
+    "domain": "5gvci.com",
+    "zoneId": 10231154
 }
 self.lary = ""
-importScripts('https://shoukigaigoors.net/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
